346df5a35d04ab95868809d3d01e604c0a1b98bc
Nate Barner <nate.barner71@gmail.com>
2025-10-30 20:26:20 -0500

    Delete tutor_app/feedback_tester.py

--- tutor_app/feedback_tester.py
+++
@@ -1,8 +0,0 @@
-#temporary file to test the feedback module
-
-from feedback import FeedbackModule
-
-text = 'Today I seen an balloon.'
-feedback = FeedbackModule()
-print(feedback.analyze(text))
-feedback.tool.close()
